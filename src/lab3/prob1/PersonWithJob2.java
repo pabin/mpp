594dcdc52105f1afc2aa78fcae99890e201d8446
Pabin R Luitel <pabin2050@gmail.com>
2025-01-16 15:58:42 -0600

    minor update on lab3

--- src/lab3/prob1/PersonWithJob2.java
+++ src/lab3/prob1/PersonWithJob2.java
@@ -1,38 +1,28 @@
 package lab3.prob1;
 
 public class PersonWithJob2 {
-    private String name;
+    private Person person;
     private double salary;
 
     public double getSalary() {
         return salary;
     }
 
-    PersonWithJob2(String n, double s) {
-        name = n;
+    PersonWithJob2(Person p, double s) {
+        person = p;
         salary = s;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object aPerson) {
-        if (aPerson == null) return false;
-        if (!(aPerson instanceof PersonWithJob2)) return false;
-        PersonWithJob2 p = (PersonWithJob2) aPerson;
-        boolean isEqual = this.name.equals(p.getName()) &&
-                this.getSalary() == p.getSalary();
-        return isEqual;
+    public Person getPerson() {
+        return person;
     }
 
     public static void main(String[] args) {
-        PersonWithJob2 p1 = new PersonWithJob2("Joe", 30000);
         Person p2 = new Person("Joe");
+        PersonWithJob2 p1 = new PersonWithJob2(p2, 30000);
         //As PersonsWithJobs, p1 should be equal to p2
-        System.out.println("p1.equals(p2)? " + p1.equals(p2));
-        System.out.println("p2.equals(p1)? " + p2.equals(p1));
+        System.out.println("p1.equals(p2)? " + p1.person.getName().equals(p2.getName()));
+        System.out.println("p2.equals(p1)? " + p2.getName().equals(p1.person.getName()));
     }
 
 
