8cd4f56fab90897aaf56ac4d94208ec440fe069e
Pabin R Luitel <pabin2050@gmail.com>
2025-01-17 16:40:26 -0600

    add initial code for lab5

---
+++ src/lab5/prob1/rulesets/RuleException.java
@@ -0,0 +1,10 @@
+//@SuppressWarnings("serial")
+//final public class RuleException extends Exception {
+//    public RuleException() {
+//        super();
+//    }
+//
+//    public RuleException(String msg) {
+//        super(msg);
+//    }
+//}
