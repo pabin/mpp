d05e189020ffcb15b66ab88595b6c8544b1a7abd
Pabin R Luitel <pabin2050@gmail.com>
2025-01-20 19:44:39 -0600

    fix: lab2 prob2b

--- src/lab2/prob2B/Order.java
+++ src/lab2/prob2B/Order.java
@@ -1,12 +1,16 @@
 package lab2.prob2B;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
     private int orderNum;
-    private OrderLine orderLine;
+    private List<OrderLine> orderLines = new ArrayList<>();
+
 
     public Order(int on, String item, int qty, double price) {
-        OrderLine orderLineItem = new OrderLine(item, qty, price);
+        OrderLine orderLineItem = new OrderLine(this, item, qty, price);
         this.orderNum = on;
-        this.orderLine = orderLineItem;
+        orderLines.add(orderLineItem);
     }
 }
--- src/lab2/prob2B/OrderLine.java
+++ src/lab2/prob2B/OrderLine.java
@@ -4,10 +4,12 @@ public class OrderLine {
     private String itemName;
     private int quantity;
     private double price;
+    private Order order;
 
-    OrderLine(String i, int q, double p) {
+    OrderLine(Order order, String i, int q, double p) {
         this.itemName = i;
         this.quantity = q;
         this.price = p;
+        this.order = order;
     }
 }
