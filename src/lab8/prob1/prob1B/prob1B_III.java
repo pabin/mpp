7bfefbd079b6d3d889ac3cb354495e0f0bf7dc5c
Pabin R Luitel <pabin2050@gmail.com>
2025-01-25 15:56:01 -0600

    add lab8 prob1B solution

--- src/lab8/prob1/prob1B/prob1B_III.java
+++ src/lab8/prob1/prob1B/prob1B_III.java
@@ -1,14 +1,19 @@
 package lab8.prob1.prob1B;
 
+import java.util.function.Supplier;
+
 public class prob1B_III {
     public static void main(String[] args) {
-
-        
+        RandomSupplier randomSupplier = new RandomSupplier();
+        double randomNumber = randomSupplier.get();
+        System.out.println("Random Number: " + randomNumber);
     }
 
-    public class RandomGenerator {
-        public static double generateRandom() {
+    static class RandomSupplier implements Supplier<Double> {
+        @Override
+        public Double get() {
             return Math.random();
         }
     }
+
 }
